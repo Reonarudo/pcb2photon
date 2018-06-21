301daa654630c2d16abc38fc30535d8747b0b569
Leonardo Marques <leonardo.j.marques@gmail.com>
2018-06-21 16:29:49 +0100

    Swift installation script cleanup

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -3,15 +3,16 @@ set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "Installing Swift for Linux"
+    sudo rm /var/lib/apt/lists/lock -qq
     sudo apt-get install clang libicu-dev git -qq
-    if [[ $OS_VER == '16.04' ]]; then
-        echo ">Ubuntu Xenial 16.04"
-    elif [[ $OS_VER == '14.04' ]]; then
+    if [[ $OS_VER == '14.04' ]]; then
         echo ">Ubuntu Trusty 14.04"
         echo "-Fetching & Installing Ubuntu Trusty 14.04 dependencies..."
         sudo apt-get install clang-3.6 -qq
         sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100 -qq
         sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100 -qq
+    else
+        echo ">Ubuntu Xenial 16.04"
     fi
     echo "-Installing Swift through #SwiftEnv#"
     mkdir .swiftenv
