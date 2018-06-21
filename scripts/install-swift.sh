d17cd0a05238c7bb0169b4f99cc0d1ad0094f41e
Leonardo Marques <leonardo.j.marques@gmail.com>
2018-06-21 17:20:25 +0100

    testing more aggressive lock removal

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -3,14 +3,18 @@ set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "Installing Swift for Linux"
+    echo "...removing dependencie locks"
     sudo rm /var/lib/apt/lists/lock
-    sudo apt-get install clang libicu-dev git -qq
+    sudo rm /var/cache/apt/archives/lock
+    sudo rm /var/lib/dpkg/lock
+    echo "...intalling dependencies"
+    sudo apt-get install clang libicu-dev git
     if [[ $OS_VER == '14.04' ]]; then
         echo ">Ubuntu Trusty 14.04"
         echo "-Fetching & Installing Ubuntu Trusty 14.04 dependencies..."
         sudo apt-get install clang-3.6 -qq
-        sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100 -qq
-        sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100 -qq
+        sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
+        sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
     else
         echo ">Ubuntu Xenial 16.04"
     fi
