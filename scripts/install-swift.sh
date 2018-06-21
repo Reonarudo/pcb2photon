68e6833acaad155598742efaa1147d59e362cf00
Leonardo Marques <leonardo.j.marques@gmail.com>
2018-06-21 14:05:42 +0100

    Added missing dowload signatures command

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -18,6 +18,7 @@ if [[ $platform == 'linux' ]]; then
     sudo apt-get install clang libicu-dev libpython2.7
     wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
     wget https://swift.org/builds/swift-$swift_v-release/ubuntu1604/swift-$swift_v-RELEASE/swift-$swift_v-RELEASE-ubuntu16.04.tar.gz
+    wget https://swift.org/builds/swift-$swift_v-release/ubuntu1604/swift-$swift_v-RELEASE/swift-$swift_v-RELEASE-ubuntu16.04.tar.gz.sig
     gpg --verify swift-$swift_v-RELEASE-ubuntu16.04.tar.gz.sig
     tar xzf swift-$swift_v-RELEASE-ubuntu16.04.tar.gz
     mv swift-$swift_v-RELEASE-ubuntu16.04 /usr/share/swift
