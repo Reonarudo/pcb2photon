da6f88bb161ce1b73a75b24ee2dee96839d07243
Leonardo Marques <leonardo.j.marques@gmail.com>
2018-06-21 14:22:25 +0100

    Cleaned travis swift instalation script and added funcionality descriptors.

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-set -ve
+set -e
 swift_v='4.0.3'
-platform='unknown'^
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-    echo "Running Linux"
-    platform='linux'
-else
-    echo "Not running Linux"
-    platform='macos'
-fi
 
-if [[ $platform == 'linux' ]]; then
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "Installing Swift"
+    echo "-Installing dependencies..."
     sudo apt-get install git -qq
     sudo apt-get update
     sudo apt-get install clang libicu-dev libpython2.7
+    echo "-Fetching Packages dependencies..."
     wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
     wget https://swift.org/builds/swift-$swift_v-release/ubuntu1604/swift-$swift_v-RELEASE/swift-$swift_v-RELEASE-ubuntu16.04.tar.gz
     wget https://swift.org/builds/swift-$swift_v-release/ubuntu1604/swift-$swift_v-RELEASE/swift-$swift_v-RELEASE-ubuntu16.04.tar.gz.sig
+    echo "-Verifying packages..."
     gpg --verify swift-$swift_v-RELEASE-ubuntu16.04.tar.gz.sig
+    echo "-Extracting packages"
     tar xzf swift-$swift_v-RELEASE-ubuntu16.04.tar.gz
+    echo "-Configureing paths..."
     mv swift-$swift_v-RELEASE-ubuntu16.04 /usr/share/swift
     echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
     source  ~/.bashrc
-    echo "Finished installing Swift"
+    echo "Finished installing Swift!"
 fi
