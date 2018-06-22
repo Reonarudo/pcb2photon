1f7efa575d3d2a3d1e5779bca6360b744d8f1378
Leonardo Marques <leonardo.j.marques@gmail.com>
2018-06-22 14:38:13 +0100

    Added external swiftenv script dependency

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ve
-cat ~/.bashrc
 echo "Installing Swift for Linux"
 echo "-Ubuntu Trusty 14.04"
 echo "      Updating, Fetching & Installing Ubuntu Trusty 14.04 dependencies..."
@@ -9,18 +8,15 @@ sudo apt-get -y install clang libicu-dev git
 sudo apt-get -y install clang-3.6
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
-source ~/.bashrc
 echo "-Installing Swift using SwiftEnv"
 echo "      Downloading SwiftEnv"
 mkdir .swiftenv
 git clone https://github.com/kylef/swiftenv ~/.swiftenv
-echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bashrc
-echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(swiftenv init -)"' >> ~/.bashrc
-source ~/.bashrc
-sudo apt-get -y install curl
-swiftenv install $SWIFT_VER
-swiftenv which swift
-
-source ~/.bashrc
+export SWIFTENV_ROOT="$HOME/.swiftenv"
+export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
+if [ -f ".swift-version" ] || [ -n "$SWIFT_VER" ]; then
+  swiftenv install -s
+else
+  swiftenv rehash
+fi
 echo "Finished installing Swift!"
